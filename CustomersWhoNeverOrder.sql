8b42118ffc6cb29f64a11292910514cfce4451be
Justin Seitz <justinseitz2016@gmail.com>
2021-07-15 16:40:07 -0700

    CustomersWhoNeverOrder solution

---
+++ CustomersWhoNeverOrder.sql
@@ -0,0 +1,19 @@
+-- Write a SQL query to find all customers who never order anything.
+-- Table from LeetCode
+Create table If Not Exists Customers (Id int, Name varchar(255));
+Create table If Not Exists Orders (Id int, CustomerId int);
+Truncate table Customers;
+insert into Customers (Id, Name) values ('1', 'Joe');
+insert into Customers (Id, Name) values ('2', 'Henry');
+insert into Customers (Id, Name) values ('3', 'Sam');
+insert into Customers (Id, Name) values ('4', 'Max');
+Truncate table Orders;
+insert into Orders (Id, CustomerId) values ('1', '3');
+insert into Orders (Id, CustomerId) values ('2', '1');
+
+-- Solution
+SELECT Name AS Customers
+FROM Customers
+LEFT JOIN Orders
+	ON Customers.Id = Orders.CustomerId
+WHERE Orders.CustomerId IS NULL;
